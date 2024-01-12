34fbad10301ab2f7ab75a1637058ef31fee066a9
euniceumutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-11 22:01:52 -0600

    Add files via upload

---
+++ web.js
@@ -0,0 +1,12 @@
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+console.log(n);
+
+if (n > 70 ) {
+   alert(a +  " and " + b + " have " + n + " % match " + ",  you were made for each other!");
+} else {
+    alert(a +  " and " + b + " have " + n + " % match " + ", maybe move on? ");
+}
+
+// var loveCalculator = prompt( a +  " and " + b + " have " + n + " % match ");
