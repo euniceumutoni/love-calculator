e66480760f24c3ee3827ed6a58b801272fa72bbd
euniceumutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-11 22:14:13 -0600

    web.js

--- web.js
+++ web.js
@@ -3,10 +3,17 @@ n = n * 100;
 n = Math.floor(n) + 1;
 console.log(n);
 
-if (n > 70 ) {
-   alert(a +  " and " + b + " have " + n + " % match " + ",  you were made for each other!");
-} else {
-    alert(a +  " and " + b + " have " + n + " % match " + ", maybe move on? ");
+if (n >= 70 ) {
+   alert(a +  " and " + b + " have " + n + " % match" + ",  you were made for each other!");
+   
+} 
+
+if (n > 30 && n <= 70) {
+   alert(a +  " and " + b + " have " + n + " % match" + ",  there's still a chance don't give up!");
+   
 }
 
-// var loveCalculator = prompt( a +  " and " + b + " have " + n + " % match ");
+if (n <= 30) {
+    alert(a +  " and " + b + " have " + n + " % match" + ", maybe move on? ");
+   
+}
